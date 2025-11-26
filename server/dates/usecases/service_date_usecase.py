e3d7e50e52e7a2ef1f2418b805fcb7ea5ec14c5e
AlexgDevs <asmetanin014@gmail.com>
2025-11-26 16:19:29 +0500

    feat: created generation schedule for all active templates services

--- server/dates/usecases/service_date_usecase.py
+++ server/dates/usecases/service_date_usecase.py
@@ -1,3 +1,5 @@
+from ast import Dict
+from datetime import timedelta, datetime
 from typing import List
 
 from dotenv.main import logger
@@ -13,8 +15,11 @@ from ...common.db import (
     select,
     selectinload,
     Service,
-    ServiceDate
+    ServiceDate,
+    ScheduleTemplate
 )
+
+from ...scheduletemplates.repositories import get_schedule_template_repository, ScheduleTemplateRepository
 
 from ..repositories import (
     ServiceDateRepository,
@@ -60,6 +65,84 @@ class ServiceDateUseCase:
             logger.error('error', f'failed creating service date, detail: {str(e)}')
             return {'status': 'failed creating service date', 'detail': str(e)}
 
+
+class DatesInteractionTemplates:
+    def __init__(
+        self,
+        session: AsyncSession,
+        service_date_repository: ServiceDateRepository,
+        schedule_template_repository: ScheduleTemplateRepository
+        ) -> None:
+
+        self._session = session
+        self._service_date_repository = service_date_repository
+        self._schedule_template_respository = schedule_template_repository
+
+    def _get_week_dates(self, start_date: datetime) -> Dict[str, str]:
+        week_dates = {}
+        for i in range(7):
+            current_date = start_date + timedelta(days=i)
+            day_name = current_date.strftime("%A").lower()
+            date_str = current_date.strftime("%Y-%m-%d")
+            week_dates[day_name] = date_str
+        return week_dates
+
+    def _get_next_sunday(self) -> datetime:
+        today = datetime.now().date()
+        days_until_sunday = (6 - today.weekday()) % 7
+        next_sunday = today + timedelta(days=days_until_sunday)
+        return datetime.combine(next_sunday, datetime.min.time())
+
+    async def generate_schedule(self):
+        templates = await self._schedule_template_respository.get_all()
+
+        next_sunday = self._get_next_sunday()
+        week_dates = self._get_week_dates(next_sunday)
+
+        completed_templates = []
+        failed = 0
+
+        for template in templates:
+            if not template.is_active:
+                continue
+        
+            try:
+                new_date = await self._create_date_with_template(
+                        template,
+                        week_dates.get(template.day)
+                    )
+                await self._session.commit()
+                completed_templates.append(new_date)
+            except SQLAlchemyError as e:
+                await self._session.rollback()
+                failed += 1
+                logger('error', f'failed creating date with template, detail: {str(e)}')
+                continue
+
+        return {'status': 'created', 'succes': len(completed_templates), 'failed': failed}
+
+    async def _create_date_with_template(
+        self,
+        template: ScheduleTemplate,
+        target_date: str
+        ):
+
+        date_model = CreateServiceDate(
+            date=target_date,
+            slots=template.hours_work,
+            service_id=template.service_id
+        )
+
+        try:
+            new_date = await self._service_date_repository.create_date(
+                date_model
+            )
+            await self._session.commit()
+            return new_date
+        except SQLAlchemyError as e:
+            await self._session.rollback()
+            logger.error('error', f'failed creating service date, detail: {str(e)}')
+            return None
 
 def get_service_date_use_case(
     session: AsyncSession = Depends(db_config.session),
